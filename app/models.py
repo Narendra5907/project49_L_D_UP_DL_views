f0502cc40ed14661b8e23590129e1ecd92973488
Narendra5907 <narendranare5907@gmail.com>
2023-05-18 22:46:09 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class School(models.Model):
+    Sname=models.CharField(max_length=100)
+    principal=models.CharField(max_length=100)
+    location=models.CharField(max_length=100)
+    def __str__(self):
+        return self.Sname
+
+class Student(models.Model):
+    school=models.ForeignKey(School,on_delete=models.CASCADE,related_name='students')
+    sname=models.CharField(max_length=100)
+    sage=models.IntegerField()
+    def __str__(self):
+        return self.sname
---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from app.models import *
+from django.views.generic import TemplateView
+from django.views.generic import ListView
+class Home(TemplateView):
+    template_name='app/Home.html'
+
+
+
+class School_list(ListView):
+    model=School
+    # template_name='app/school_list.html'
+    context_object_name='schools'
